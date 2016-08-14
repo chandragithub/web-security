664336ef761dc66c2cdbd930b26dcde58ba1df2b
Chandra Shekher Polimera <chandrashekher@hotmail.com>
2016-08-14 15:30:26 +0530

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,137 @@
-# web-security
-Securing web application by preventing cross site scripting (XSS) attack across the browsers.
+## Web-Security
+   Securing web application by preventing cross site scripting (XSS) attack across the browsers.
+
+## Copyright (randomrise.com)
+      Copyright © http://randomrise.com, as an published work.  All rights reserved.
+      This software is the property of randomrise technology
+      This software intend to provide open source (FREE TO USE) but any reproduction is not allowed
+
+      @contact: http://randomrise.com 
+      @author: ChandraShekher Polimera (linkedin: chandrashekherpolimera | email: chandrashekher@techie.com)
+      @github: https://github.com/chandragithub/xssescape
+      @date: 13/08/2016
+      @version: 0.0.1 (beta)
+ 
+## Description: 
+   It prevents cross site scripting (xss) attacts across the browser.
+### what is a cross site scripting attack (xss)?
+   Cross-site scripting (XSS) is a code injection attack that allows an attacker to execute malicious JavaScript in another user's browser
+
+### Types of XSS attack?
+
+      -  Persistent XSS
+         where the malicious string originates from the website's database.
+      
+      -  Reflected XSS
+         where the malicious string originates from the victim's request.
+      
+      -  DOM-based XSS
+         where the vulnerability is in the client-side code rather than the server-side code.
+
+
+## Prevention Method:
+
+    - @param: unSafeHtmlString
+      escape(param) {};
+
+    - @param: unSafeHtmlString
+      strictEscape(param) {};
+
+    - @param: safeHtmlString
+      reverseEscape(param) {};
+
+    - @param: safeHtmlString
+      unescape(param) {};
+
+    - @param: unSafeHtmlString
+      removeUnsafe(param) {};
+
+    - safeUrl() {};
+
+    - unSafeUrl() {};
+
+
+## Installation
+
+```sh
+npm install webSecurity
+```
+
+## API
+
+```js
+var xs = require('webSecurity')
+```
+
+## Usage.
+### escape
+
+```js
+   var htmlStr = "<script> alert(document.cookie); </script>";
+   xs.escape(htmlStr);
+   
+   // output: "&lt;script&gt alert(document.cookie); &lt/script&gt"
+```
+
+### strictEscape
+
+```js
+   var htmlStr = "<script> alert(document.cookie); </script>";
+   xs.strictEscape(htmlStr);
+   
+   // output: "&lt;script&gt alert&#40;document.cookie&#41;&#59; &lt/script&gt"
+```
+
+### reverseEscape
+
+```js
+   var htmlStr = "&lt;script&gt alert&#40;document.cookie&#41;&#59; &lt/script&gt";
+   xs.reverseEscape(htmlStr);
+   
+   // output: "<script> alert(document.cookie); </script>"
+```
+
+### unescape
+
+```js
+   var htmlStr = "<script> alert(document.cookie); </script>";
+   xs.unescape(htmlStr);
+   
+   // output: "<script> alert(document.cookie); </script>"
+```
+
+### removeUnsafe
+
+```js
+   var htmlStr = "<script> alert(document.cookie); </script>";
+   xs.removeUnsafe(htmlStr);
+   
+   // output: "script alert(document.cookie); /script"
+```
+
+### removeStrictUnsafe
+
+```js
+   var htmlStr = "<script> alert(document.cookie); </script>";
+   xs.removeStrictUnsafe(htmlStr);
+   
+   // output: "script alertdocument.cookie /script"
+```
+
+### safeUrl
+ 
+```js
+   var url = "http://randomrise.com/?<script> document.cookie </script>";
+   xs.safeUrl(url);
+   
+   // it will reload/refresh the page without search parameter.
+```
+
+### unSafeUrl
+ 
+```js
+   var url = "http://randomrise.com/?<script> document.cookie </script>";
+   xs.unSafeUrl(url);
+   
+   // it will reload/refresh the page  with search parameter.
+```
---
+++ index.js
@@ -0,0 +1,166 @@
+/**
+ *  Copyright © http://randomrise.com, as an published work.  All rights reserved.
+ *  THIS SOFTWARE OR DATA IS THE PROPERTY OF http://randomrise.com.
+ *  THIS SOFTWARE INTENTD TO PROVIDE OPEN SOURCE (FREE TO USE) BUT ANY REPRODUCTION NOT ALLOWED.
+ *  CONTACT: http://randomrise.com
+ *
+ * @author ChandraShekher Polimera (linkedin: chandrashekherpolimera | email: chandrashekher@techie.com)
+ * @date 13/08/2016
+ * @version 0.0.1 (beta)
+ */
+
+var webSecurity = (function () 
+{
+    var webSecurity = {};
+    
+    webSecurity.escape = function (htmlStr) 
+    {
+      if (htmlStr === undefined) 
+      {
+          return null;
+      }
+      else
+      {
+        return String(htmlStr)
+          .replace(/&/g, '&amp;')
+          .replace(/"/g, '&quot;')
+          .replace(/'/g, '&#39;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;');
+      }
+    };
+    
+    webSecurity.strictEscape = function(htmlStr)
+    {
+       if (htmlStr === undefined) 
+       {
+          return null;
+       }
+       else
+       {
+        return String(htmlStr)
+          .replace(/&/g, '&amp;')
+          .replace(/"/g, '&quot;')
+          .replace(/'/g, '&#39;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
+          .replace(/\%/g, '&#37;')
+          .replace(/\;/g, '&#59;')
+          .replace(/\(/g, '&#40;')
+          .replace(/\)/g, '&#41;')
+          .replace(/\+/g, '&#43;')
+          .replace(/\-/g, '&#45;');
+       } 
+    };
+    
+    
+    webSecurity.reverseEscape = function(htmlStr) 
+    {
+      if (htmlStr === undefined) 
+      {
+          return null;
+      }
+      else
+      {
+         return String(htmlStr)
+          .replace(/&amp;/g, '&')
+          .replace(/&quot;/g, '"')
+          .replace(/&#39;/g, "'")
+          .replace(/&lt;/g, '<')
+          .replace(/&gt;/g, '>')
+          .replace(/&#37;/g, '%')
+          .replace(/&#59;/g, ';')
+          .replace(/&#40;/g, '(')
+          .replace(/&#41;/g, ')')
+          .replace(/&#43;/g, '+')
+          .replace(/&#45;/g, '-');
+      }
+    };
+    
+    webSecurity.unescape = function(htmlStr) 
+    {
+      if (htmlStr === undefined) 
+      {
+          return null;
+      }
+      else
+      {
+         return htmlStr;
+      }
+
+    };
+    
+    webSecurity.removeUnsafe = function(htmlStr)
+    {
+       if (htmlStr === undefined) 
+       {
+          return null;
+       }
+       else
+       {
+         return String(htmlStr)
+          .replace(/&/g, '')
+          .replace(/"/g, '')
+          .replace(/'/g, '')
+          .replace(/</g, '')
+          .replace(/>/g, '');
+       }
+    };
+    
+    webSecurity.removeStrictUnsafe = function(htmlStr)
+    {
+       if (htmlStr === undefined) 
+       {
+          return null;
+       }
+       else
+       {
+        return String(htmlStr)
+          .replace(/&/g, '')
+          .replace(/"/g, '')
+          .replace(/'/g, '')
+          .replace(/</g, '')
+          .replace(/>/g, '')
+          .replace(/\%/g, '')
+          .replace(/\;/g, '')
+          .replace(/\(/g, '')
+          .replace(/\)/g, '')
+          .replace(/\+/g, '')
+          .replace(/\-/g, '');
+       } 
+    };
+
+    webSecurity.safeUrl = function()
+    {
+        if(typeof window !== 'undefined')
+        {
+           var searchLocation =  location.search;
+           var unSafeChar = searchLocation.match(/[script]/g);
+              
+           ((unSafeChar !== null) ? (unSafeChar.length > 0 ? location.replace("/") : "") : "");
+             
+        }
+        else
+        {
+           throw "Node Doesn't Support safeUrl";
+        }
+    };
+    
+    webSecurity.unSafeUrl = function()
+    {
+        var searchLocation =  location.search;
+        location.replace(searchLocation);
+    };
+     
+    return webSecurity;
+    
+}());
+
+if (typeof window === 'undefined') 
+{
+    module.exports = webSecurity;
+}
+else 
+{
+    window.webSecurity = webSecurity;
+};
